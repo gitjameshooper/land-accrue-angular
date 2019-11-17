00022ff78e9fb1adeadaa6fd2f6f8087aedaf9bd
gitjameshooper <jamesthomashooper@gmail.com>
2019-11-17 11:34:33 -0600

    Bug Problem Found

--- client/tasks/clean-data.js
+++ client/tasks/clean-data.js
@@ -26,6 +26,7 @@ function formatSoldData(csv) {
             'LATITUDE': o['LATITUDE'],
             'LONGITUDE': o['LONGITUDE'],
             'URL': o['URL (SEE http://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)'],
+            'distance': 0
         })
 
 
@@ -58,6 +59,7 @@ function formatBuyData(csv) {
             'id': 0,
             'avgPPA': 0,
             'avgPPA2': 0,
+            'avgPPA3': 0,
             'estValue': 0,
             'estValue2': 0,
             'estValue3': 0,
@@ -118,19 +120,18 @@ function distance(lat1, lon1, lat2, lon2, unit) {
 
 function mergeData(buyData, soldData, offerData) {
     let dupArr = [],
-       uniqueId = 1;
+        uniqueId = 1,
+        BD = [];
     _.forEach(buyData, (bv, bk) => {
         let soldArr = [],
-            ppArr = [],
-            pricePerAcreArr = [],
-            totalPricePerAcreM1 = 0,
-            totalPricePerAcreM2 = 0,
+            totalPricePerAcre = 0,
             offerObj = _.find(offerData, { 'SITUS FULL ADDRESS': buyData[bk]['SITUS FULL ADDRESS'] });
-        buyData[bk]['id'] = uniqueId++;
+            buyData[bk]['id'] = uniqueId++;
+
         // 8 miles radius from buy property - Loops through each mile adding to the sold array
         _.forEach([0, 1, 2, 3, 4, 5, 6, 7, 8], (v, k) => {
             _.forEach(soldData, (sv, sk) => {
-                let d = distance(bv['LATITUDE'], bv['LONGITUDE'], sv['LATITUDE'], sv['LONGITUDE'], 'M'),
+                let soldDistance = distance(bv['LATITUDE'], bv['LONGITUDE'], sv['LATITUDE'], sv['LONGITUDE'], 'M'),
                     minAcre = buyData[bk]['LOT ACREAGE'] - 1,
                     maxAcre = buyData[bk]['LOT ACREAGE'] + 1;
                 if (buyData[bk]['LOT ACREAGE'] > 12) {
@@ -153,26 +154,70 @@ function mergeData(buyData, soldData, offerData) {
                     minAcre = buyData[bk]['LOT ACREAGE'] - .25;
                     maxAcre = buyData[bk]['LOT ACREAGE'] + .25;
                 }
-
+              
                 // Less than 5 sold properties on array and Less than (d) miles away and close to the same lot acreage
-                if (soldArr.length < 5 && d < v + 1 && d > v && minAcre < soldData[sk]['LOT ACREAGE'] && maxAcre > soldData[sk]['LOT ACREAGE']) {
-                    soldData[sk]['distance'] = Math.round(d * 100) / 100
-                    totalPricePerAcreM1 += soldData[sk]['PRICE PER ACRE'];
-                    pricePerAcreArr.push(soldData[sk]['PRICE PER ACRE']);
-                    soldArr.push(soldData[sk]);
+                if (soldArr.length < 5 && soldDistance < v + 1 && soldDistance > v && minAcre < soldData[sk]['LOT ACREAGE'] && maxAcre > soldData[sk]['LOT ACREAGE']) {
+                  let soldProp = soldData.slice();
+                    soldProp[sk]['distance'] = Math.round(soldDistance * 100) / 100;
+                        // console.log('BUY:'+buyData[bk]['SITUS FULL ADDRESS']);
+                        // console.log(soldProp[sk]['ADDRESS']+'-'+soldDistance);
+                        // console.log('-------------------');
+                    totalPricePerAcre += soldData[sk]['PRICE PER ACRE'];
+                    soldArr.push(soldProp[sk]);
+
+                  
+                    // console.log(soldArr);
                 }
             });
         });
+        buyData[bk]['soldArr'] = [];
+          buyData[bk]['soldArr'] = soldArr;
+            console.log('-------------------');
+              console.log('-------------------');
+                console.log('-------------------');
+        
 
-        ppArr = pricePerAcreArr.length < 10 ? _(pricePerAcreArr).sortBy().take(Math.round(pricePerAcreArr.length / 2)).value() : _(pricePerAcreArr).sortBy().take(Math.round(pricePerAcreArr.length / 4)).value();
+        // Top 3 Closests by distance: Properties Price per Acre
+        let sortedDistanceSoldArr = _.sortBy(soldArr, ['distance']);
+        let totalPPADistance = 0;
 
 
-        buyData[bk]['avgPPA'] = ppArr.length > 0 ? Math.round(_.sum(ppArr) / ppArr.length) : 0;
-        buyData[bk]['avgPPA2'] = totalPricePerAcreM1 > 0 ? Math.round(totalPricePerAcreM1 / soldArr.length) : 0;
+        if (sortedDistanceSoldArr[0] && sortedDistanceSoldArr[1] && sortedDistanceSoldArr[2]) {
+            totalPPADistance = sortedDistanceSoldArr[0]['PRICE PER ACRE'] + sortedDistanceSoldArr[1]['PRICE PER ACRE'] + sortedDistanceSoldArr[2]['PRICE PER ACRE'];
+        } else if (sortedDistanceSoldArr[0] && sortedDistanceSoldArr[1]) {
+            totalPPADistance = sortedDistanceSoldArr[0]['PRICE PER ACRE'] + sortedDistanceSoldArr[1]['PRICE PER ACRE'];
+        } else if (sortedDistanceSoldArr[0]) {
+            totalPPADistance = sortedDistanceSoldArr[0]['PRICE PER ACRE'];
+        }
+
+        // if (buyData[bk]['SITUS FULL ADDRESS'] === '7998 FM 539 SUTHERLAND SPRINGS,TX 78161') {
+        //     console.log(sortedDistanceSoldArr);
+        //     console.log(totalPPADistance);
+        // }
+        buyData[bk]['avgPPA'] = totalPPADistance / 3;
+
+
+        // 3 middle with avg. Price per Acre
+        let sortedPPASoldArr = _.sortBy(soldArr, ['PRICE PER ACRE']);
+
+        let totalPPAMiddle = 0;
+        if (sortedPPASoldArr[1] && sortedPPASoldArr[2] && sortedPPASoldArr[3]) {
+            totalPPAMiddle = sortedPPASoldArr[1]['PRICE PER ACRE'] + sortedPPASoldArr[2]['PRICE PER ACRE'] + sortedPPASoldArr[3]['PRICE PER ACRE'];
+        } else if (sortedPPASoldArr[1] && sortedPPASoldArr[2]) {
+            totalPPAMiddle = sortedPPASoldArr[1]['PRICE PER ACRE'] + sortedPPASoldArr[2]['PRICE PER ACRE'];
+        } else if (sortedPPASoldArr[1]) {
+            totalPPAMiddle = sortedPPASoldArr[1]['PRICE PER ACRE'];
+        }
+        buyData[bk]['avgPPA2'] = totalPPAMiddle / 3;
+
+        // All 5 avg. Price per Acre
+        buyData[bk]['avgPPA3'] = totalPricePerAcre > 0 ? Math.round(totalPricePerAcre / soldArr.length) : 0;
+
         // Flood Zone TRUE-> discount 50%
         if (buyData[bk]['IN FLOOD ZONE']) {
             buyData[bk]['avgPPA'] *= .50;
             buyData[bk]['avgPPA2'] *= .50;
+            buyData[bk]['avgPPA3'] *= .50;
         }
 
         // Calculates Estimated Values and Offers
@@ -202,33 +247,42 @@ function mergeData(buyData, soldData, offerData) {
         if (buyData[bk]['marketValueFlag']) {
             buyData[bk]['statusColor'] = 'red';
         }
+        if (buyData[bk]['SITUS FULL ADDRESS'] === '7998 FM 539 SUTHERLAND SPRINGS,TX 78161') {
+            console.log(sortedDistanceSoldArr);
 
-        buyData[bk]['soldArr'] = soldArr;
-        
+        }
+       
+
         // Remove Duplicates: Choose from the most sold data for each property
         _.forEach(buyData, (bv2, bk2) => {
             if (buyData[bk2]['soldArr'] && buyData[bk]['MAILING STREET ADDRESS'] === buyData[bk2]['MAILING STREET ADDRESS'] && buyData[bk]['SITUS FULL ADDRESS'] !== buyData[bk2]['SITUS FULL ADDRESS']) {
-              
-                if(buyData[bk]['soldArr'].length === buyData[bk2]['soldArr'].length){
-                   dupArr.push(buyData[bk]);
-                } else if(buyData[bk]['soldArr'].length < buyData[bk2]['soldArr'].length){
-                  dupArr.push(buyData[bk]);
-                } else if(buyData[bk]['soldArr'].length > buyData[bk2]['soldArr'].length){
-                  dupArr.push(buyData[bk2]);
-                } 
+
+                if (buyData[bk]['soldArr'].length === buyData[bk2]['soldArr'].length) {
+                    dupArr.push(buyData[bk]);
+                } else if (buyData[bk]['soldArr'].length < buyData[bk2]['soldArr'].length) {
+                    dupArr.push(buyData[bk]);
+                } else if (buyData[bk]['soldArr'].length > buyData[bk2]['soldArr'].length) {
+                    dupArr.push(buyData[bk2]);
+                }
             }
 
         });
- 
+        BD.push(buyData[bk]);
+// console.log(BD[bk]);
     });
+
     // Removes Duplicate Mailing addresses filteredBuyData : buyData
-    let filteredBuyData = _.differenceBy(buyData, dupArr, 'id');  
-    createJSONFile(filteredBuyData, 'total');
+    let filteredBuyData = _.differenceBy(buyData, dupArr, 'id');
+    
+        console.log(BD[2].soldArr);
+    console.log('--------------');
+        console.log(BD[4].soldArr);
+    // createJSONFile(filteredBuyData, 'total');
 
 }
 
 // Read and Write Files
-let buyCSVData = fs.readFileSync('./csv/buy.csv', 'utf8');
+let buyCSVData = fs.readFileSync('./csv/buy2.csv', 'utf8');
 buyCSVData = d3.csvParse(buyCSVData);
 
 let soldCSVData = fs.readFileSync('./csv/sold.csv', 'utf8');
